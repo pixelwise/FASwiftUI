5d04a46f5c60b57c5bbfee61a91d4793bcda4d60
Cyril Rutten <crutten@gmail.com>
2020-01-25 13:49:56 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "FASwiftUI",
     platforms: [
         .iOS(.v13)
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
