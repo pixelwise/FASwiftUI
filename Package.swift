48235a1191f1bf98da510d6e078b99bd50cb1e3a
Cyril Rutten <crutten@gmail.com>
2020-04-11 18:31:38 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FASwiftUI",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v10_15),
         .watchOS(.v6)
     ],
     products: [
